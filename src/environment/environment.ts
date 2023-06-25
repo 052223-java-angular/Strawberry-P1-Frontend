d6d8ebf4b616d8e7778a7537f47f8b378a70cd27
bduong0929 <bao.duong@revature.com>
2023-06-24 21:02:09 -0400

    Can log in using auth 0

---
+++ src/environment/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    baseUrl: 'http://localhost:8080/strawberry/api',
+    auth: {
+        domain: 'dev--kmy-w6c.us.auth0.com',
+        clientId: 'kIOt5XX1kxkc1nQVxQQih9UkLM1kSYXy',
+    }
+}
